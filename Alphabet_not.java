6b77b42094dfc6955a3998d481edcc9b7a807bd1
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-06-28 20:36:52 +0530

    Add files via upload

---
+++ Alphabet_not.java
@@ -0,0 +1,15 @@
+package Hari;
+import java.util.*;
+public class Alphabet_not {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		char n=sc.next().charAt(0);
+		if((n>='a'&&n<='z')||(n>='A'&&n<='Z'))
+			System.out.println("Yes");
+			else
+				System.out.println("No");
+		
+		
+	}
+
+}
