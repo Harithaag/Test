190715687b81b16874c3b41ad8e5adf73447feaa
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-06-28 23:12:16 +0530

    Add files via upload

---
+++ Prime_number.java
@@ -0,0 +1,26 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Prime_number{
+
+public static void main (String s[]){
+	int i=0,n;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("num");
+	n=sc.nextInt();
+	for(int j=2;j<n;j++){
+	if(n%j==1){
+		i=1;
+	break;
+	}}
+	if(i==0){
+		System.out.println("Yes");
+	}
+		else{
+			System.out.println("No");
+		
+	}
+	
+}
+}
