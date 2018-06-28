ee63fa29585895e8895a25978effa03f171103a5
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-06-28 22:18:05 +0530

    Add files via upload

---
+++ Count_num_dig.java
@@ -0,0 +1,10 @@
+package Hari;
+import java.util.*;
+public class Count_num_dig {
+public static void main(String s[]){
+	Scanner sc=new Scanner(System.in);
+	String str=sc.nextLine();
+	System.out.println(str.length());
+	
+}
+}
