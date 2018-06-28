77be4a4fea4a70df7f49dabccf5443bafbf01959
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-06-28 17:49:30 +0530

    Add files via upload

---
+++ Amstrong.java
@@ -0,0 +1,23 @@
+package Hari;
+import java.util.Scanner;
+
+public class Amstrong {
+	public static void main (String s[]){
+		int r,su=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=n;
+		while(a!=0){
+			r=a%10;
+			a=a/10;
+			su+=r*r*r;
+		}
+		if(n==su){
+			System.out.println("A");
+		}
+			else{
+				System.out.println("nA");
+		}
+	}
+
+}
