d5c360112e42b151260092465f1c7d692f85c9b3
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-06-28 21:39:35 +0530

    Add files via upload

---
+++ N_time.java
@@ -0,0 +1,15 @@
+package Hari;
+import java.util.*;
+public class N_time {
+	public static void main(String s[]){
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		String str="Hello";
+		for(int i=1;i<=n;i++){
+			System.out.println(str);
+		}
+
+		
+	}
+
+}
