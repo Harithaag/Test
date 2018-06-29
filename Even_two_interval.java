06786ebc3a81ee3c40b94d75f95c83944c037c54
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-06-29 10:32:35 +0530

    Add files via upload

---
+++ Even_two_interval.java
@@ -0,0 +1,19 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Even_two_interval {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		for(int i=a;i<b;i++){
+			
+				
+			
+			if(i!=1&&i%2==0)
+				System.out.println(i);
+		}
+	}
+
+}
