e9ba165903da6e03a5335e7184f7580196139f83
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-06-29 15:10:36 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Multiplication {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=5;i++){
+			int s1=n*i;
+			System.out.println(s1);
+					
+		}
+	}
+
+}
