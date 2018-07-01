dd68dadd4181a5660c5e8a3750c5f435cec5a433
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-01 09:15:05 +0530

    Add files via upload

---
+++ Time_min_hr.java
@@ -0,0 +1,11 @@
+package Hari;
+import java.util.*;
+public class Time_min_hr {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(n/60+" "+n%60);
+		
+	}
+
+}
