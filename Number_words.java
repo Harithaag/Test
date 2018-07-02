a9e862be4cb13be25aba9db32744f6faf04bc180
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-02 22:56:30 +0530

    Add files via upload

---
+++ Number_words.java
@@ -0,0 +1,11 @@
+package Hari;
+import java.util.*;
+public class Number_words {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String[] str1=str.split(" ");
+		System.out.println(str1.length);
+	}
+
+}
