bbfab9bd6fcc060c2b7a7030b9daaf2f54f17bed
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-02 22:05:18 +0530

    Add files via upload

---
+++ Count_character.java
@@ -0,0 +1,16 @@
+package Hari;
+import java.util.*;
+class Count_character {
+	public static void main(String s[]){
+		Scanner sc =new Scanner(System.in);
+		String str=sc.nextLine();
+		String str1=str.replace(" ","");
+	
+		//System.out.print(str1);
+       // int str2=i;
+		System.out.println(str1.length());
+		
+				
+	}
+
+}
