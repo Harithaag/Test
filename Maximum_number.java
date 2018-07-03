6c92d49acb29db298be6ef71669c69b8e2be0cd9
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-03 23:30:40 +0530

    Add files via upload

---
+++ Maximum_number.java
@@ -0,0 +1,21 @@
+package Hari;
+import java.util.*;
+public class Maximum_number {
+	public static void main(String s[]){
+		int m ;
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[10];
+		for(int i=0;i<10;i++){
+			a[i]=sc.nextInt();
+				
+			}
+		m=a[0];
+		for(int i=0;i<10;i++){
+			
+			if(m<a[i]){
+				m=a[i];
+		
+	}}
+		System.out.println(m);
+}
+}
