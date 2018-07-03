986919d7ca471057f2aeec39ac8f2d84a049a411
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-03 12:42:35 +0530

    Add files via upload

---
+++ Numeric_character.java
@@ -0,0 +1,20 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Numeric_character {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int f=0;
+		for(int i=0;i<str.length();i++)
+		{
+	//if(str.charAt(i))
+			if(Character.isDigit(str.charAt(i))){
+				f++;
+			}
+		}
+		System.out.println(f);
+	}
+
+}
