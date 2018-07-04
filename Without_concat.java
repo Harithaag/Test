359bf3bf51460e7d9d9cfd67e806a9813b23a08f
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-04 22:48:47 +0530

    Add files via upload

---
+++ Without_concat.java
@@ -0,0 +1,16 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Without_concat {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String str1=str.replace(" ","");
+		System.out.println(str1);
+			
+		}
+		
+	}
+
+
