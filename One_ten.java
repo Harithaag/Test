f75955fd902f9829fde46aef0593929737f2a7d8
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-04 23:00:12 +0530

    Add files via upload

---
+++ One_ten.java
@@ -0,0 +1,16 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class One_ten {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<=10&&n>=1)
+			System.out.println("Yes");
+		
+		else
+			System.out.println("No");
+	}
+
+}
