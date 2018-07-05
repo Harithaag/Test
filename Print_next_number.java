c17fca43f86c9c69e0a185c6728f4b4b946a5a4d
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-05 11:18:42 +0530

    Add files via upload

---
+++ Print_next_number.java
@@ -0,0 +1,12 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Print_next_number {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(n+1);
+	}
+
+}
