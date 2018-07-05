107e28e48f84841a5afe7c54b11bfbd2a30457c0
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-05 11:13:45 +0530

    Add files via upload

---
+++ Count_the_numberic.java
@@ -0,0 +1,16 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Count_the_numberic {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		
+		String n=sc.nextLine();
+		
+		//if(Character.isDigit(n.charAt(i)))
+		System.out.println(n.length());
+
+}
+
+}
