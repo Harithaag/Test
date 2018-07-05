ec31377ea0fdd00e110a5db529b7ab46686f8d5e
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-05 12:07:39 +0530

    Add files via upload

---
+++ Sum_two_num.java
@@ -0,0 +1,13 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Sum_two_num {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		System.out.println(n+m);
+	}
+
+}
