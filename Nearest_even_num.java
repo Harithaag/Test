17654243b30cef0427f5372b81d93b724874cee4
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-12 15:22:26 +0530

    Add files via upload

---
+++ Nearest_even_num.java
@@ -0,0 +1,15 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Nearest_even_num {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%2==0)
+			System.out.println(n);
+			else
+		
+		System.out.println(n-1);
+	}
+}
