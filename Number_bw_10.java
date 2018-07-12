597a1e0543b4358f9d1f4b63146c560f67f23f85
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-12 15:11:56 +0530

    Add files via upload

---
+++ Number_bw_10.java
@@ -0,0 +1,14 @@
+package Hari;
+import java.util.*;
+public class Number_bw_10 {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String str[]={" ","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"};
+				
+	if(n<=10){
+		System.out.print(str[n]);
+	}
+
+}
+}
