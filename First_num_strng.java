eb022f1789afae6bae59e1e6400d1dbba5504a72
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-17 09:47:57 +0530

    Add files via upload

---
+++ First_num_strng.java
@@ -0,0 +1,14 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class First_num_strng {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		int k=sc.nextInt();
+		System.out.println(str.substring(0,k));
+		
+	}
+
+}
