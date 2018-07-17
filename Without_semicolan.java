b1c77e19d02c122c7bada0854baa00057a0aa5b5
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-17 21:53:56 +0530

    Add files via upload

---
+++ Without_semicolan.java
@@ -0,0 +1,14 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Without_semicolan {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		if(System.out.printf(str)==null){
+			
+		}
+	}
+
+}
