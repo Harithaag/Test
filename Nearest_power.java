656b75f243cd1da0bf0075a7394ff1de1a015cbd
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-17 13:16:45 +0530

    Add files via upload

---
+++ Nearest_power.java
@@ -0,0 +1,12 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Nearest_power {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(2*n);
+
+}
+}
