f4c18eeed51dc760fe3facfd2373b5f1b79e8f9f
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-07-17 13:20:05 +0530

    Add files via upload

---
+++ Multiple_seven.java
@@ -0,0 +1,16 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Multiple_seven {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%7==0)
+		System.out.println("yes");
+		else
+			System.out.println("no");
+
+}
+
+}
