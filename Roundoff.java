0a49fbf1c88ee4abfcbdeafa54365c3de9e4d5e9
Harithaag <40663951+Harithaag@users.noreply.github.com>
2018-08-02 10:14:26 +0530

    Add files via upload

---
+++ Roundoff.java
@@ -0,0 +1,15 @@
+package Hari;
+
+import java.util.Scanner;
+
+public class Roundoff {
+	public static void main(String s[]){
+		Scanner sc=new Scanner(System.in);
+		float a=sc.nextFloat();
+		double b=Math.ceil(a);
+		int c=(int)b;
+		System.out.println(c);
+		
+		
+	}
+}
